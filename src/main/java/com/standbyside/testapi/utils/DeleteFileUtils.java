26eef34a6dd7d271653e00c931bcf0b385723c63
Jerry <smart1jerry@outlook.com>
2019-12-31 13:52:30 +0800

    [feat] add delete file utils

---
+++ src/main/java/com/standbyside/testapi/utils/DeleteFileUtils.java
@@ -0,0 +1,25 @@
+package com.standbyside.testapi.utils;
+
+import java.io.File;
+
+public class DeleteFileUtils {
+
+    /**
+     * 删除除html外的所有文件.
+     */
+    public static void deleteFilesExcludeHtml(String folderPath) {
+        System.out.println("enter " + folderPath);
+        File[] files = new File(folderPath).listFiles();
+        for (File file : files) {
+            if (file.getName().endsWith(".html")) {
+                continue;
+            }
+            if (file.isDirectory()) {
+                deleteFilesExcludeHtml(file.getAbsolutePath());
+            } else {
+                System.out.println("delete " + folderPath);
+                file.deleteOnExit();
+            }
+        }
+    }
+}
